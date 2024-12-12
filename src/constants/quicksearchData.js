0fdd1d4f5a741c6dd7fe14c846a33dab5e43ed06
Gokarna Adhikari <agokarna444@gmail.com>
2024-12-12 15:20:14 +0545

    quick search data dummy data

---
+++ src/constants/quicksearchData.js
@@ -0,0 +1,10 @@
+
+export const quicksearchData = [
+    { id: 1, img: "https://cdn.pixabay.com/photo/2015/11/06/11/48/multi-family-home-1026488_640.jpg", name: "Apartments", propertyFound: "2000+", link: "/properties/apartments" },
+    { id: 2, img: "https://cdn.pixabay.com/photo/2015/11/06/11/39/single-family-home-1026369_640.jpg", name: "Villas", propertyFound: "2000+", link: "/properties/villas" },
+    { id: 3, img: "https://cdn.pixabay.com/photo/2016/08/16/03/50/exterior-1597098_640.jpg", name: "Houses", propertyFound: "2000+", link: "/properties/houses" },
+    { id: 4, img: "https://cdn.pixabay.com/photo/2019/09/15/14/22/fishermans-hut-4478427_640.jpg", name: "Rents", propertyFound: "2000+", link: "/properties/offices" },
+    { id: 5, img: "https://cdn.pixabay.com/photo/2015/07/02/09/52/interior-design-828545_640.jpg", name: "Offices", propertyFound: "2000+", link: "/properties/offices" },
+    { id: 6, img: "https://cdn.pixabay.com/photo/2013/11/01/19/31/rice-fields-204139_640.jpg", name: "Lands", propertyFound: "2000+", link: "/properties/offices" },
+
+];
