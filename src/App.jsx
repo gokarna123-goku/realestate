92220be631e9d8b552e4db6738ebd09beeb126cc
Gokarna Adhikari <agokarna444@gmail.com>
2024-12-21 22:40:53 +0545

    first initialize the project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <>
+      <main className="w-full min-h-screen bg-neutral-50 flex flex-col">
+        This is the main section.
+      </main>
+    </>
+  )
+}
+
+export default App
